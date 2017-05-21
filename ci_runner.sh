87af165b9e452438320b839a7142f5188d30b88c
fresheed <ok.john.rus@gmail.com>
2017-05-22 00:12:54 +0300

    minor refactor

--- ci_runner.sh
+++ ci_runner.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
-python3 -m parse.ci_load_logs --token $dropbox_token --local_dir raw_logs
+local_logs_dir=raw_logs
+python3 -m parse.ci_load_logs --token $dropbox_token --local_dir $local_logs_dir
+echo "Files loaded to $local_logs_dir"
 python3 -m unittest discover
