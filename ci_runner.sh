244749ce289e8933f6900eacad301fe20006e8db
fresheed <ok.john.rus@gmail.com>
2017-05-22 02:48:24 +0300

    added all experiments run to ci_runner

--- ci_runner.sh
+++ ci_runner.sh
@@ -1,7 +1,15 @@
 #! /bin/sh
 
-python3 -m parse.ci_load_logs --token $dropbox_token \
+python3 -m parse.ci_load_logs \
+    --token $dropbox_token \
     --local_dir $local_logs_dir \
     --archive_name $archive_name
 echo "Files loaded to $local_logs_dir"
+
 python3 -m unittest discover
+
+python3 -m classification.experiments.run_experiment \
+    --algorithm all \
+    --log_dir $local_logs_dir \
+    --classes pushups5_ walk50_ sits10_ typing_
+
