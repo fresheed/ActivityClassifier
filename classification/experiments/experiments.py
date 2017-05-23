92b0433eb0efe9a6e5262248291501b42e3ea6eb
fresheed <ok.john.rus@gmail.com>
2017-05-23 14:05:51 +0300

    now 4 stratified folds are used in experiments

--- classification/experiments/experiments.py
+++ classification/experiments/experiments.py
@@ -30,7 +30,8 @@ class Experiment(object):
 
         params=dict(transformer_params)
         params.update(classifier_params)
-        searcher=GridSearchCV(pipeline, scoring="f1_macro", param_grid=params)
+        searcher=GridSearchCVe(pipeline, scoring="f1_macro", 
+                               param_grid=params, cv=4)
 
         train_items, train_classes=zip(*train_set)
         test_items, test_classes=zip(*test_set)
