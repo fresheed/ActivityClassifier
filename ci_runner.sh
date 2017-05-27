d38d1d974be556dc59b637d06bc1a21e1ca9730f
fresheed <ok.john.rus@gmail.com>
2017-05-28 00:17:19 +0300

    back to exercises recognition

--- ci_runner.sh
+++ ci_runner.sh
@@ -11,8 +11,9 @@ python3 -m unittest discover
 python3 -m classification.experiments.run_experiment \
     --algorithm all \
     --log_dir $local_logs_dir \
-    --classes bigramNA bigramST bigramTO
+    --classes pushups5_ walk50_ sits10_ typing_
 
 #--classes pushups5_ walk50_ sits10_ typing_
-#    --classes letterA letterB letterV
+#--classes bigramNA bigramST bigramTO
+#--classes letterA letterB letterV
 
