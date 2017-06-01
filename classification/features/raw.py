c6535f02672e8c739e6d431679f3fad628f201b2
fresheed <ok.john.rus@gmail.com>
2017-06-01 20:20:43 +0300

    removed excessive logs

--- classification/features/raw.py
+++ classification/features/raw.py
@@ -9,7 +9,6 @@ class RawExtractor(LogFeatureExtractor):
         axes_coeffs=[self.process_single_axis(item[axis])
                      for axis in ("x", "y", "z")]        
         all_coeffs=list(itertools.chain.from_iterable(axes_coeffs))
-        print("len:", len(all_coeffs))
         return all_coeffs
 
     def process_single_axis(self, item):
